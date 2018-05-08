7888228796b3e865bc749f619e363ee4fd5db61d
hellen <15652531860@163.com>
2018-05-08 21:38:58 +0800

    feat: add fingerprint to get user browser id & count spam times by browser id and ip

--- app/main/views.py
+++ app/main/views.py
@@ -9,15 +9,18 @@ from . import main
 @main.route("/", methods=["GET","POST"])
 def index():
     browser_id = request.args.get('id')
-    print(browser_id)
+    user_ip = request.remote_addr
     form = CommentForm()
     if form.validate_on_submit():
         if not current_user.is_authenticated:
             return redirect(url_for('main.login'))
-        if redis.get(current_user.username + '_comment'):
-            return redirect(url_for('main.index'))
-        redis.set(current_user.username + '_comment', True)
-        redis.expire(current_user.username, 5)
+        if redis.get(browser_id):
+            if int(redis.get(browser_id)) > 3 or int(redis.get(user_ip)) > 3:
+                return redirect(url_for('main.login'))
+        redis.incr(browser_id)
+        redis.incr(user_ip)
+        redis.expire(browser_id, 30)
+        redis.expire(user_ip, 30)
         comment = Comment(content=form.content.data, user=current_user._get_current_object())
         db.session.add(comment)
         return redirect(url_for('main.index'))
@@ -26,18 +29,21 @@ def index():
 
 @main.route('/login', methods=['GET','POST'])
 def login():
+    browser_id = request.args.get('id')
+    user_ip = request.remote_addr
     form = LoginForm()
     if form.validate_on_submit():
+        if redis.get(browser_id):
+            if int(redis.get(browser_id)) > 3 or int(redis.get(user_ip)) > 3:
+                return redirect(url_for('main.login'))
+        redis.incr(browser_id)
+        redis.incr(user_ip)
+        redis.expire(browser_id, 30)
+        redis.expire(user_ip, 30)
         user = User.query.filter_by(username=form.username.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)          
-            if redis.get(user.username + '_login'):
-                if int(redis.get(user.username+ '_login')) > 3:
-                    return redirect(url_for('main.login'))
-            redis.incr(user.username + '_login')
-            redis.expire(user.username + '_login', 30)
             return redirect(request.args.get("next") or url_for("main.index"))
-        flash('Invalid username or password')
     return render_template("login.html", form=form)
 
 @main.route('/logout')
