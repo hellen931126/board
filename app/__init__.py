e232996edf269ace702787fd57038949078e785f
hellen <15652531860@163.com>
2018-05-09 18:53:44 +0800

    fix: remove pagedown

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_pagedown import PageDown
 from flask_redis import FlaskRedis
 from config import config
 
@@ -11,7 +10,6 @@ bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 login_manager = LoginManager()
-pagedown = PageDown()
 redis = FlaskRedis()
 login_manager.session_protection = "basic"
 login_manager.login_view = 'login'
@@ -29,5 +27,4 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     redis.init_app(app)
-    pagedown.init_app(app)
     return app
